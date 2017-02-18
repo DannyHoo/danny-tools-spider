d34122aea5fe271b27d75db300c3d930eb1cbf47
yuyang.hu <huyuyang@lxfintech.com>
2017-02-18 16:09:59 +0800

    create StringUtils

---
+++ common-utils/src/main/java/top/danny/spider/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package top.danny.spider.utils;
+
+/**
+ * @author huyuyang@lxfintech.com
+ * @Title: StringUtils
+ * @Copyright: Copyright (c) 2016
+ * @Description:
+ * @Company: lxjr.com
+ * @Created on 2017-02-18 16:09:00
+ */
+public class StringUtils {
+}
