d29fdd7f3bf4a51bf168970be6121664c62c5cd0
yuyang.hu <huyuyang@lxfintech.com>
2017-02-18 16:13:18 +0800

    edit name

--- common-utils/src/main/java/top/danny/spider/utils/StringUtilsDanny.java
+++ common-utils/src/main/java/top/danny/spider/utils/StringUtilsDanny.java
@@ -2,11 +2,11 @@ package top.danny.spider.utils;
 
 /**
  * @author huyuyang@lxfintech.com
- * @Title: StringUtils
+ * @Title: StringUtilsDanny
  * @Copyright: Copyright (c) 2016
  * @Description:
  * @Company: lxjr.com
  * @Created on 2017-02-18 16:09:00
  */
-public class StringUtils {
+public class StringUtilsDanny {
 }
